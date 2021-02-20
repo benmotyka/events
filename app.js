df4fc1605c1beb308eac4f3ba4a7dcfd12075392
ben <rikenpl99@gmail.com>
2021-02-20 11:25:44 +0100

    2. setting up db and mongoose models

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-import express from "express"
-import bodyParser from "body-parser"
-import dotenv from 'dotenv'
-import {graphqlHTTP} from "express-graphql"; // middleware
-import { buildSchema } from 'graphql'
-dotenv.config()
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { graphqlHTTP } from "express-graphql"; // middleware
+import { buildSchema } from "graphql";
+import Event from "./models/event.js";
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     //! - required
     schema: buildSchema(`
 
-    type RootQuery {
-        events: [String!]! 
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
     }
 
-    type RootMutation {
-        createEvent(name: String): String
+        type RootQuery {
+            events: [Event!]!
+        }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+    
+    type RootMutation{
+        createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -27,19 +46,52 @@ app.use('/graphql', graphqlHTTP({
     }
     `), //valid graphql schema
     rootValue: {
-        events: () => {
-            return ['Sailing', 'Coding'];
-        },
-        createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
-        }
+      events: () => {
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc };
+
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+      createEvent: (args) => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: args.eventInput.price,
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc }; //returns only declared properties, nothing more
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+      },
     },
-    graphiql: true
-}))
+    graphiql: true,
+  })
+);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log("Api listening on port: " + port)
-})
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.la7tf.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Api listening on port: " + port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
